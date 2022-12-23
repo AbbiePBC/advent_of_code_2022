2a6cea5915ea6d75acb391bf643378fea325f83c
Abbie <abbie.currington@intrasonics.com>
2022-12-23 15:13:03 +0000

    dec_01: inefficient solution for part 2

--- dec_01/src/main.rs
+++ dec_01/src/main.rs
@@ -1,37 +1,61 @@
 use anyhow::{Context, Result};
-use std::cmp;
 use std::fs::read_to_string;
 
 fn main() {
-    let max = max_calories_for_elf("puzzle_input.txt");
-    println!("{:?}", max)
+    let calories_per_elf = read_and_get_calories_per_elf("puzzle_input.txt");
+    match calories_per_elf {
+        Ok(mut cals) => {
+            cals.sort();
+            //println!("Part One: {}", get_top_totals(cals, 1));
+            println!("Part Two: {}", get_top_totals(cals, 3));
+        },
+        Err(_) => {},
+    };
 }
 
-fn max_calories_for_elf(file_path: &str) -> Result<i64, anyhow::Error> {
+fn read_and_get_calories_per_elf(file_path: &str) -> Result<Vec<i64>, anyhow::Error> {
     let lines = read_to_string(file_path)
         .with_context(|| format!("Failed to read file from {}", file_path))?;
     let calories_for_elf: Vec<&str> =  lines.split("\n\n").collect();
-    let mut max_calories_for_elf = 0;
+    let mut calorie_count_for_elf : Vec<i64> = Vec::with_capacity(calories_for_elf.len() -1);
     for elf in calories_for_elf {
-        let all_calories_collected: Vec<&str> = elf.split("\n").collect();
-        let mut sum_for_elf = 0;
-        for calories in all_calories_collected {
-            let _ = match calories.parse::<i64>() {
-                Ok(cal) => { sum_for_elf += cal; },
-                Err(_) => {},
-            };
-        }
-        max_calories_for_elf = cmp::max(max_calories_for_elf, sum_for_elf);
+        calorie_count_for_elf.push(get_calories_for_elf(elf));
     }
-    return Ok(max_calories_for_elf);
+    return Ok(calorie_count_for_elf);
+}
+
+fn get_calories_for_elf(elf: &str) -> i64 {
+    let all_calories_collected: Vec<&str> = elf.split("\n").collect();
+    let mut sum_for_elf = 0;
+    for calories in all_calories_collected {
+        let _ = match calories.parse::<i64>() {
+            Ok(cal) => { sum_for_elf += cal; },
+            Err(_) => {},
+        };
+    }
+    return sum_for_elf
+}
+
+fn get_top_totals( cals: Vec<i64>, i: usize) -> i64 {
+    let mut total = 0;
+    let num_elves_less_1 = cals.len();
+    for i in  (num_elves_less_1 - i)..num_elves_less_1 {
+        total += cals[i];
+    }
+    return total
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::max_calories_for_elf;
+    use super::*;
     #[test]
-    fn test_file_parsing() {
-        let calorie_list_for_elf = max_calories_for_elf("test/short_test.txt");
-        println!("{:?}", calorie_list_for_elf);
+    fn test_all() {
+        let mut calories_per_elf = read_and_get_calories_per_elf("./test/short_test.txt").unwrap();
+        assert_eq!(calories_per_elf, [6000, 4000, 11000, 24000, 10000]);
+
+        calories_per_elf.sort();
+        assert_eq!(calories_per_elf, [4000, 6000, 10000, 11000, 24000]);
+        assert_eq!(get_top_totals(calories_per_elf, 3), 45000);
+
     }
 }
