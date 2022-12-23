8e95f6aa97ee31a6de28f0cf8e8101063b5a9528
Abbie <abbie.currington@intrasonics.com>
2022-12-23 21:23:53 +0000

    dec_05: part 1

--- dec_05/main.rs
+++ dec_05/main.rs
@@ -1,35 +1,38 @@
 use std::fs::read_to_string;
 
+
 const POSITION_NUM: usize = 1;
 const POSITION_START: usize = 3;
 const POSITION_END: usize = 5;
 
 fn main() {
-    let input_data = read_to_string("dec_05/test_input.txt").unwrap();
+    let input_data = read_to_string("dec_05/actual_input.txt").unwrap();
     let (stacks, instructions) = parse_input(&input_data);
-    //todo
+    let result = do_instructions(instructions, stacks);
+
+    println!("Solution = {}", get_solution(result));
+
 }
 
+
 fn parse_input(input_data: &str) -> (Vec<Vec<char>>, Vec<&str>) {
-    println!("input data = {}", input_data);
+
     let separated_data: Vec<&str> = input_data.split("\n\n").collect();
     let raw_initial_structure: Vec<&str> = separated_data[0].split("\n").collect();
     let initial_stacks = get_stacks(raw_initial_structure);
-
     let raw_instructions: Vec<&str> = separated_data[1].split("\n").collect();
-    println!("{:?}", raw_instructions);
 
     return (initial_stacks, raw_instructions);
 }
 
+
 fn get_stacks(raw_initial_structure: Vec<&str>) -> Vec<Vec<char>> {
-    println!("raw initial structre = {:?}", raw_initial_structure);
     let number_of_stacks = raw_initial_structure[raw_initial_structure.len() - 1]
         .split("   ")
         .collect::<Vec<&str>>()
         .len();
     let mut all_stacks: Vec<Vec<char>> = Vec::new();
-    for i in 0..number_of_stacks {
+    for _ in 0..number_of_stacks {
         all_stacks.push(Vec::new());
     }
     // the relevant char appears at the xth place in the string, where x = 4n + 1(zero indexed)
@@ -51,26 +54,55 @@ fn get_stacks(raw_initial_structure: Vec<&str>) -> Vec<Vec<char>> {
             }
         }
     }
-    println!("stacks = {:?}", all_stacks);
-    return all_stacks;
+
+    reverse_stacks(&mut all_stacks);
+
+    return all_stacks
+
 }
 
-fn do_instruction(instruction: &str) {
-    let (num_stacks, start_point, end_point) = parse_instruction(instruction);
-    //todo: move x ... should be one by one
+fn reverse_stacks(all_stacks: &mut Vec<Vec<char>>) {
+    for stack in all_stacks {
+        stack.reverse();
+    }
 }
 
-fn parse_instruction(instruction: &str) -> (i32, i32, i32) {
+fn do_instruction(instruction: &str, all_stacks : &mut Vec<Vec<char>>) {
+    let (num_boxes_to_move, start_point, end_point) = parse_instruction(instruction);
+    for _ in 0..num_boxes_to_move {
+        let char = all_stacks[start_point - 1].pop();
+        match char {
+            Some(ch) => { all_stacks[end_point - 1].push(ch); },
+            None => { println!("this is an error."); }
+        };
+        //todo: error handle this properly
+
+    }
+}
+
+fn parse_instruction(instruction: &str) -> (usize, usize, usize) {
     let inst: Vec<&str> = instruction.split(" ").collect();
     return (
-        inst[POSITION_NUM].parse::<i32>().unwrap(),
-        inst[POSITION_START].parse::<i32>().unwrap(),
-        inst[POSITION_END].parse::<i32>().unwrap(),
+        inst[POSITION_NUM].parse::<usize>().unwrap(),
+        inst[POSITION_START].parse::<usize>().unwrap(),
+        inst[POSITION_END].parse::<usize>().unwrap(),
     );
 }
 
-fn do_instructions() {
-    //todo: do_instruction per line of instructions
+fn do_instructions(instructions: Vec<&str>, mut all_stacks: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    for i in 0..instructions.len() -1 {
+        do_instruction(instructions[i], &mut all_stacks);
+    }
+    return all_stacks;
+}
+
+
+fn get_solution(stacks: Vec<Vec<char>>) -> String {
+    let mut answer : Vec<char> = Vec::new();
+    for stack in stacks {
+        answer.push(stack[stack.len() -1]);
+    }
+    return answer.into_iter().collect();
 }
 
 #[cfg(test)]
@@ -79,13 +111,13 @@ mod tests {
     #[test]
     fn test_stacks() {
         let result = get_stacks(vec!["    [D]", "[N] [C]", "[Z] [M] [P]", " 1   2   3"]);
-        assert_eq!(result, vec![vec!['N', 'Z'], vec!['D', 'C', 'M'], vec!['P']]);
+        assert_eq!(result, vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']]);
     }
     #[test]
     fn test_parse_input() {
         let input_data = read_to_string("dec_05/test_input.txt").unwrap();
         let (stacks, instructions) = parse_input(&input_data);
-        assert_eq!(stacks, vec![vec!['N', 'Z'], vec!['D', 'C', 'M'], vec!['P']]);
+        assert_eq!(stacks, vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']]);
         assert_eq!(
             instructions,
             [
@@ -111,9 +143,20 @@ mod tests {
     #[test]
     fn test_single_instruction() {
         // test move 1, move x
+        let mut stacks = get_stacks(vec!["    [D]", "[N] [C]", "[Z] [M] [P]", " 1   2   3"]);
+        do_instruction("move 1 from 2 to 1", &mut stacks);
+        assert_eq!(stacks, vec![vec!['Z', 'N', 'D'], vec!['M', 'C'], vec!['P']]);
+
+        do_instruction("move 3 from 1 to 3", &mut stacks);
+        assert_eq!(stacks, vec![vec![], vec!['M', 'C'], vec!['P', 'D', 'N', 'Z']]);
     }
     #[test]
     fn test_all() {
-        // test move 1, move x
+        let input_data = read_to_string("dec_05/test_input.txt").unwrap();
+        let (stacks, instructions) = parse_input(&input_data);
+        let result = do_instructions(instructions, stacks);
+
+        let ans = get_solution(result);
+        assert_eq!(ans, "CMZ");
     }
 }
