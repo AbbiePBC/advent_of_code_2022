382dd32ecdac0cb3d4d391b8e73cc83468fafec2
Abbie <ac2061@cantab.ac.uk>
2023-01-09 23:27:57 +0000

    an excuse to learn about OOP, rather than write something fast lol

--- dec_06.rs
+++ dec_06.rs
@@ -1,3 +1,105 @@
+use std::{str::Chars, collections::HashSet};
+use substring::Substring;
+
 fn main() {
-    // your subroutine needs to identify the first position where the four most recently received characters were all different
+    let signal = "rhghwwsmsgmgsmmmlzljllrddvsvhvhhnvvrcccwhhvgghchwhvwvcvrrrgtgrgdggfdgffshsllvvslsppglgvgwgswwcbcwbwrrbjbwjjtgjjdzdfdhfddrmmhqhpqhhqghhzssqzqccbwwffzvzffvtftddrbrtrrcjrrmmbmrrrlrppplmplmppfzpfpvpqvpqqdndpndnmnwmwjmwmwbbrhhmzmffwfqwfqwffppvfpvffnwwwwcbwbhbccvmmplmplmmlmplpprbpbllmhlhzhffngfnntrrsrprrvlllvjvmjvvppsrrfnrfnrfrfwrffrsfrflfltfllpfprrthhhnhqnnfwwcttvzzddqsddwpddnrnttvjttjjdjvdjvjnvnrvvchhschhlffjggtqgqnnvrnrsnrsnsvspsnncppvwvvtmmcqmcmscmsmwmvmpvvlqqmbqqlwwtgtztgttpjjbfjjzhzthhzssffwqqdmdcdlclrclrccbmmqjqmqcmmnnsvnntpphhdpdhhtbtdtffhzfhhpwhppwbppfspphdhggzqgzzffdsdfsszcscpscsqcssffhjhnhhlqqdbqqdmdsmddrfddndldgglvglvlggqlggjngjnnbffthhjbjpjnjmnmgmqqbjqjzjqzzzjffwttcnnzffmllcncmnnthnncttpgpnnjdjfftmmvqmqsqggfrfjjqbjbvvshvhphfhhljlfjjvcclzclctcmmfwfvwfvwwjvwvjjplpbpqqdvqqbhbdbnnsndssgffgsglssmfmjjfrjfrfttljldjdffjjdnjjlbbnrntrtfrttmhhndnsdsffgqqrpplvvfwvfvsfszzfzhfzzlttlctllrzrggvwvhhgnnlmnlmlpppdldqqdpdlplrpppjspspzzhphccgjjpmmwvwbwssnpssrzrwrhhmzhmhsmhmttrqrvqvcclggptpjpzpccltctppflfjffrtrjrhjhffhjfjbfjfqfqzffdtthwwjwzjzggsgzgqqjdqjddvgvsvnnqwqgwgtttfvfrfwrfwfwmwmqwmmmbzmzvzfvfrfvvgvtvptpbpwbwrbwrwffzzfgzfgfcfzcffjdjjvrvnnfmmtztffsggjcjzcjcssscnscnssmmwgwbbljlnjndnrrqwqjjjbnnmbmcmbcmbbvvmtvmmfcmcjcdcnnzfzfzqfzfvvmbvmmndnbnsnnlddqvvsnngvnnrttfbtbqqnwqqzfzfzccfmmgrrsbbhvvgjjhfhssjmmsjjzzbqblqlfqllwhlwwvvstvtfvttnncjjzcjcjrrsvrssmfsfczbrzrvscvmcmrjpzwhcqfrrzbljnmqlzbzqtmhrshlrjjpvhnsvtlhqggqwppsjpszmqwfqmlwbqzwcrggrvfbvztnflwvbrqcrqbcllswvsvhwjzpldgphwptfdnlgdlnbttjfzrdcfvpdhlssfsljvdjmwddbnrpqnnqlfdfdspbnjwqjwrgtnrftsqcfjpmqwgwhttggjwzvgbwlhmtmmjlwhssrgshzpbcnstzlqdshdhjfgqlsmqqhpwbscsjhfbhprvhmftqngjgdbcvfldqgqsjqjfdmcvsflwzflsjfssnjpbwffnsfcnrdsphbjpgghmcthgnzmpgppqjdvbztvhnwqzndntcpjdtwwhvsmgdcthpssszrqcbntgsznpghmbqddpqscntjprlwzhbzhjtwzbwwcldwdgsttzmtnstjnngzrgvhncdbgqnllfzbthldztsdwsngjzprbvrnbzrsghlgssbqfnbvhnhzwmmmtncvsdngdtwcbjlnnzbnlrnmrvnvsjnvzdqnggmsvljlvjznwdszcmblhrsjvczpnlhsmqjsmwhbjbplbtqsgqjdllhncwdgbvzwnmqvndcbfhnvtjnzmvjhwzvdldhgfwbqqzcnbflfnwntlhmqgdhmrgwqcpmsvfbmwhbtsbdlhcnbcbswvdfffgjvddrbpzpcwsrsjnfvmzhlvbdnwttnqrmzbnnntbfvptrlhjhwjcsbnhvtwtwzvgfnzjplthqjbsjjwzdtqqvblnbvgcmvrmnvmwfrhcqgvrcjlfzdlpbfvncbtfgvnsflbjzqqhczcmtbwqmrppmptfgzvfbmcslwlfrfpvvnvvnwfvvmmdzmmtjsgqdfhngtphtlfjqrtljgnthgnbbqfrnpfmpwhpzdvzmtswwdvcnpsdcqwjdwlvbsbmlwdsjbcbgcrfljshlvpngfmsrzlfhtfqgwbcctnzzhnqhdmqzdwthftwtmpbcmqvdcdtgvltbzmszzwwmhzlfvbdqnhjqgdmstsnhftcwzvvbmnhwvgqzscwcdjbdgfmvpjdzctwqwltbwjlgcblnnhpnmggbmvqpqtgqjzspgqzvcvsdbvjgjfzdzhfpbzjqljjcgldzgnlmtjcmfgdbqgglvjqrppwmhccvqzvsrjjvfhjprwdsqsnszfprznljtcsrtqhcrpljfrccflmbpvqtzgmzhjrlbnrmmsmmjbtzwpglqgdvvvjvnfzmplsmvlvcnjshvjwntclwgpznnzwhjssgdcjbzrmsgnfgcgphrhfvrfhzwdcvsplhbmqwhpmjvqlmschznbqblvhtqfgtdggmncndhhplnzjphccjmlmtdqnmnlnpnfqdstljqnsqbrjrtspvrwvdmwzlgdmsfvctzgtmgqhqqrzpbgplzcfdqnzhsqrbcvhsccshnnpvvrpvqzqsgzgmpzfvvrrcvhdtntnsqnjrbzlbzmpgwdqzbhtlrrhbwdqjlsfgdhmvgmgbqhwvljmmqfllqvvrznrlftgzjdcgtstjffqmgvffpvtctzpdqjfnmlcdzscntctmqhrtmhrlhbjzttrcvcnlhsrvtpbmdchhntnnpnzlvqqnsrjcmblcvphqgwshnjmplgvnbsmmdzgqcpqztjhhgjvtlbpdpdwlwmmfrdgcvzfbgvbgpbjnwsssvhszwplcgvpgjwdrwngbcdjwvlsfhqlrqzgrzpfgjstqfdbrpqdvrlgdwqcsrgvhctznjjlzsmzctsqtfnhhlpjgnltssglmlwshfbrgmjqbvsmqwvszdfsvhmtrfjgwjctpsmgzzjbpwsztnnvzrhwvvmhdpgdmwzsjprhlgzcdvhznlfgjqvcwqrplcfvzmthsdsnrtfvnlrmvwplmbdvdggmlvpgdgzhvzmvzwmptzsnfrcrjspccmqjpjmhjqgrjbdcdjbzjmphmcdvjqtmdshhjrqgjgsnpzfbfgpjpczwzvmclgzgztlvzmdbwgncnndjwhhhjnhtjdmcnrmnqbmjdrdcmtvcsmftqcfhsvhsfjmtzjpnwffggpfqlqmzlbhnnhbtgzfgnjvdzmvthqjrhzbwvhcjzcsmsvsctrqbltpcrpjjnsbjdbfjqfcbpcgcwtqsflmlwprjcwlmcjjgsfdpwcqvhjpsgvdgsfnscnbzsrmrbbvdrlltzplbvgqsdnplcvbhddbtmwnfmvqhqdlrtrmrmzmhlccgwgmbdppjqdjtwmvdzfsbsggrfstjwjpjnljffwffmqncfnthnhglwvsgvzmgbzhtdfpfmdwmcldthvsnqnptpmhqctblgfsszhcfbvcrggjdhthqvvvlldshvqwmvdtfslrhzvgdfwztrczdjgcfcgtmwnphqthlgpfnrqwcgpzwnlgdvsnvzftlnlfflfsmjzhrhqjctsbvtccwbfsdrnbhszzjhqndvwcsmffnstnfdfwpbgfztjmjngdczzlgpscjtshpmmmzlnqndsttbdgfjqcvbqlphwhlhgcvjbhjmtrfzlgpwdnvzrllndbhvhlngvhlszzdcrdgvrmjwcvhhtbhnjmdzgctqnpdlrnqjzbchjtcsggsczlgmvtqvzmsqvtrhtvdmzlcdddfnbvbsnrzvgzfqjtbhjqhdznrhbfbqwtnwvrfqsznbqfzfzfgmhvjjsgbbdbdtzswwlnfrq";
+    println!("part 1 {:?}", signal.get_marker_position(4));
+    println!("part 2 {:?}", signal.get_marker_position(14));
+
+}
+
+
+trait Signal {
+    fn get_buffer(&self,  start_idx: usize, buffer_length: usize) -> Vec<char>;
+    fn get_marker_position(&self, buffer_length: usize) -> usize;
+
+}
+
+impl Signal for &str {
+    fn get_buffer(&self, start_idx: usize, buffer_length: usize) -> Vec<char> {
+        return self.substring(start_idx,  start_idx + buffer_length).chars().collect();
+    }  
+    fn get_marker_position(&self, buffer_length: usize) -> usize {
+        let mut marker_idx = 0;
+        let mut buffer = self.get_buffer(marker_idx, buffer_length);
+        let string_chars : Vec<char> = self.chars().collect();
+        // todo can optimise checks by separating filling the buffer and comparing new things in
+        // so that the checks each time we slide the buffer along = 3, rather than 3!
+        while marker_idx + buffer_length < string_chars.len() {
+            
+            buffer = self.get_buffer(marker_idx, buffer_length);
+            // todo had planned to pop and push rather than recreate vector but rust doesn't let me 
+            // pop from the front of the vec and i have this function now anyway
+            if buffer.all_chars_differ() { return marker_idx + buffer_length; }
+            marker_idx += 1;
+            
+        }
+    
+        return 0;
+    }
+}
+
+trait RepetitionCheck {
+    fn all_chars_differ(&self) -> bool;
+}
+
+impl RepetitionCheck for Vec<char>{
+    fn all_chars_differ(&self) -> bool {
+        // honestly just cba to check properly
+        let mut set  : HashSet<char> = HashSet::new();
+        for ch in self {
+            set.insert(*ch);
+        }
+        return set.len() == self.len()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_buffer(){
+        let signal = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        let initial_buffer = ['m', 'j', 'q', 'j'];
+        assert_eq!(signal.get_buffer(0, 4), initial_buffer);
+        let second_buffer = ['j', 'q', 'j', 'p'];
+        assert_eq!(signal.get_buffer(1, 4), second_buffer);
+
+    }
+    #[test]
+    fn test_to_add_new_unique_char(){
+        assert_eq!(vec!['m', 'j', 'q', 'j'].all_chars_differ(), false);
+        
+    }
+    #[test]
+    fn test_marker_position() {
+        let signal_1= "nppdvjthqldpwncqszvftbrmjlhg";
+        assert_eq!(signal_1.get_marker_position(4), 6);
+    
+        let signal_2= "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        assert_eq!(signal_2.get_marker_position(4), 5);
+
+        let signal_3= "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        assert_eq!(signal_3.get_marker_position(4), 10);
+
+        let signal_4= "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        assert_eq!(signal_4.get_marker_position(4), 11);
+    }
+    #[test]
+    fn test_marker_position_part_two() {
+        let signal_1= "nppdvjthqldpwncqszvftbrmjlhg";
+        assert_eq!(signal_1.get_marker_position(14), 23);
+    
+        let signal_2= "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        assert_eq!(signal_2.get_marker_position(14), 23);
+
+        let signal_3= "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        assert_eq!(signal_3.get_marker_position(14), 29);
+
+        let signal_4= "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        assert_eq!(signal_4.get_marker_position(14), 26);
+
+    }
+    
 }
